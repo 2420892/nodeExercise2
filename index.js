09dac5124313bba61150c52b29dc3b6e8ffc2c89
siyamanga22 <zwenisiyamanga@gmail.com>
2023-07-31 14:51:53 +0200

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+const person = require('./person')
+const person1 = new person ('Aphelele','Joyi','aphelelejoyi@gmail.com')
+const person2 = new person('Joel','Mukanya','joelmukanya@gmail.com')
+const person3 = new person('kamva','sikalana','sikalanak@yahoo.com');
+console.log('First place:', person1);
+console.log('2nd place:', person2);
+console.log('3rd place:', person3);
---
+++ person.js
@@ -0,0 +1,10 @@
+function Person(firstName, lastName, emailAddress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = emailAddress;
+  }
+  
+  module.exports = Person;
+  
+
+
